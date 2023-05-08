aae30f856e2f33351e5af791b0ebe416e3e191ff
shabirmean <shabirmean@google.com>
2023-05-08 10:51:48 -0400

    cleanup: remove unnecessary semicolon

--- infra/modules/spanner/sql-schema/labels.sql
+++ infra/modules/spanner/sql-schema/labels.sql
@@ -2,4 +2,4 @@ CREATE TABLE labels (
     item_id STRING(36) NOT NULL,
     labels STRING(255),
     CONSTRAINT FK_items_labels FOREIGN KEY (item_id) REFERENCES items (id)
-) PRIMARY KEY (item_id);
+) PRIMARY KEY (item_id)
