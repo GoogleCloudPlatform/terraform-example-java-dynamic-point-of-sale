fd789abfaf610b995b8dfc5f0ce9f49755d02f65
Don McCasland <donmccasland@google.com>
2023-07-26 14:13:08 -0700

    Update versions.tf

--- infra/test/setup/versions.tf
+++ infra/test/setup/versions.tf
@@ -19,11 +19,9 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.25.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 3.25.0"
     }
   }
 }
